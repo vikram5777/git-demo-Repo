e4e4ecbe37a626625b3c86eea5032340ebf4b300
Vikram <vs5670@gmail>
2024-03-09 18:59:26 +0530

    www commit

--- src/main/java/com/springview1/NewProjectmvc1Application.java
+++ src/main/java/com/springview1/NewProjectmvc1Application.java
@@ -13,23 +13,29 @@ public class NewProjectmvc1Application {
 	public static void main(String[] args) {
 		
 	ConfigurableApplicationContext run = SpringApplication.run(NewProjectmvc1Application.class, args);
-		    UserRepositrary bean = run.getBean(UserRepositrary.class);
-		       User u = new User();
-		         u.setCity("delhi");
-		         u.setName("amit");
-		       //  User save = bean.save(u);
-		         User u1 = new User();
-		         u.setCity("satna");
-		         u.setName("john");
-		         List<User> c= new ArrayList<>();
-		          c.add(u1);
-		          c.add(u);
-		         
-		         List<User> saveAll = bean.saveAll(c);
-		         for(Object s:saveAll) {
-		        	 
-		        	 System.out.println(s);
-		         }
+
+	  UserRepositrary bean = run.getBean(UserRepositrary.class);
+	/* User u = new User(); u.setCity("delhi"); u.setName("amit"); // User save = bean.save(u);
+	 * User u1 = new User(); u.setCity("satna"); u.setName("john"); List<User> c=
+	 * new ArrayList<>(); c.add(u1); c.add(u);
+	 * 
+	 * List<User> saveAll = bean.saveAll(c); for(Object s:saveAll) {
+	 * 
+	 * System.out.println(s); } 	 
+	 */
+//	  List<User> byName = bean.findByName("Amit");
+//	   byName.stream().forEach(user->System.out.println(user));
+//	    List<User> list = bean.findByNameAndCity("amit", "rewa");
+//	      list.stream().forEach(user->System.out.println(user));
+//	      
+	//List<User> list = bean.findByNameStartingWith("a");
+	 //  list.stream().forEach(user->System.out.println(user));
+//	  List<User> all = bean.getAll();
+//	    all.stream().forEach(a->System.out.println(a));
+	   // List<User> list = bean.deleteNull();
+	   // list.stream().forEach(b->System.out.println(b));
+	    List<User> name = bean.findByName("john");
+	    name.stream().forEach(b->System.out.println(b));
 	}
 
 }
--- src/main/java/com/springview1/UserRepositrary.java
+++ src/main/java/com/springview1/UserRepositrary.java
@@ -1,7 +1,26 @@
 package com.springview1;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepositrary extends JpaRepository<User, Integer> {
 
+//	 public List<User> findByName(String name);
+//	 public List<User>findByNameAndCity(String name,String city);
+//	 public List<User>findByNameStartingWith(String prifix);
+	 
+	 @Query("select u FROM User u")
+	 public List<User> getAll();
+	 
+		/*
+		 * @Query(value="DELETE FROM User WHERE name IS NULL",nativeQuery=true) public
+		 * List<User> deleteNull();
+		 */
+//	 @Query(value =" select name from User where name=:name",nativeQuery=true)
+//	 public List<User> getName(@Param("name")String name);
+	 @Query(value = "SELECT * FROM users WHERE name =?", nativeQuery = true)
+	 List<User> findByName(String name);
 }
